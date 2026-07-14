48033dea79afa0fa08da9878fcf71023d392b8b9
Yahaya Bello <yahayadgl@gmail.com>
2026-07-14 01:18:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
+function sendMessage() {
 
-function sendMessage(){
+    let message = document.getElementById("message").value.toLowerCase();
+    let reply = "";
 
-let message=document.getElementById("message").value;
+    if (message.includes("python")) {
+        reply = "Python yare ne mai sauƙin koya kuma yana da amfani wajen gina AI.";
+    }
 
-if(message===""){
-    return;
-}
+    else if (message.includes("ai")) {
+        reply = "AI yana taimakawa mutane su warware matsaloli cikin sauri da hikima.";
+    }
 
-document.getElementById("chat").innerHTML += `
-<p><b>Kai:</b> ${message}</p>
+    else if (message.includes("nexora")) {
+        reply = "NEXORA AI an gina ta domin taimaka wa mutane su samu mafita da ilimi.";
+    }
 
-<p><b>NEXORA AI:</b>
-Na karɓi saƙonka.
-Na samu mafita.
-Na samu malami.
-</p>
-`;
+    else {
+        reply = "Na karɓi saƙonka. Zan ci gaba da koyo domin in fi taimaka maka.";
+    }
 
-document.getElementById("message").value="";
+    document.getElementById("chat").innerHTML += `
+        <p><b>Kai:</b> ${message}</p>
+
+        <p><b>NEXORA AI:</b> ${reply}</p>
+    `;
+
+    document.getElementById("message").value = "";
 }
