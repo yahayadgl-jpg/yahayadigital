44c6304c8549c43336c52a1045ed95172fd71eaa
Yahaya Bello <yahayadgl@gmail.com>
2026-07-14 06:27:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let chatHistory = [];
+
 function sendMessage() {
 
     let input = document.getElementById("message");
@@ -16,23 +18,33 @@ function sendMessage() {
         reply = "AI yana taimakawa mutane su warware matsaloli.";
     }
     else if (message.toLowerCase().includes("nexora")) {
-        reply = "NEXORA AI tana da manufar taimaka wa mutane su samu ilimi da mafita.";
+        reply = "NEXORA AI tana da manufar taimaka wa mutane su samu mafita da ilimi.";
     }
     else {
-        reply = "Na gode da saƙonka. Har yanzu ina koyo, amma zan ci gaba da inganta amsoshina.";
+        reply = "Na gode da saƙonka. Zan ci gaba da koyo domin in fi taimaka maka.";
     }
 
-    let chat = document.getElementById("chat");
+    chatHistory.push({
+        user: message,
+        ai: reply
+    });
 
-    chat.innerHTML += `
+    let html = "";
+
+    for (let chat of chatHistory) {
+
+        html += `
         <hr>
 
-        <p><strong>Kai:</strong> ${message}</p>
+        <p><strong>Kai:</strong> ${chat.user}</p>
 
-        <p><strong>NEXORA AI:</strong> ${reply}</p>
-    `;
+        <p><strong>NEXORA AI:</strong> ${chat.ai}</p>
+        `;
+    }
+
+    document.getElementById("chat").innerHTML = html;
 
     input.value = "";
-
     input.focus();
+
 }
