c48df1d7608069b4da32314f512ba23d1a23672c
Yahaya Bello <yahayadgl@gmail.com>
2026-07-16 06:53:40 +0100

    Update API_KEY in script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ main{max-width:900px;margin:0 auto;padding:10px 14px 100px}
 </main>
 
 <script>
-const API_KEY ="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"
+const API_KEY = "AQ.Ab8RN6LWKqGzmvwRSG9aA_xP-RrfCZnkFlBjiHCmBDJrUrHgag"
 // ============================================================
 
 const messagesEl = document.getElementById('msgs');
